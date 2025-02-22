d3b0a179d6efcd32a657d1fd55c6583fe67ddd86
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-22 10:26:51 +0530

    [Pranjal_Haswani]add. Logger to Log Messages.

--- src/main/java/com/example/SpringCoreConcepts/HelloController.java
+++ src/main/java/com/example/SpringCoreConcepts/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.SpringCoreConcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    //Correct SLF4J Logger
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, Spring Boot!";
     }
+
+    //UC2: Hello World with Logging
+    @GetMapping("/uc2")
+    public String sayHelloUC2() {
+        logger.info("GET request received at /hello/uc2");
+        return "Hello from BridgeLabz with Logging";
+    }
+
 }
